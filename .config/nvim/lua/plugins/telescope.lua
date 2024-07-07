756259f87fd481b1f91223803df8e5fbfac7181c
Byron Smith <68315031+Estanz0@users.noreply.github.com>
2024-07-07 23:57:04 +1200

    telescope config

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -40,6 +40,11 @@ require("telescope").setup({
         height = 0.5,
       },
       -- Other telescope configuration options
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
     },
   },
 })
